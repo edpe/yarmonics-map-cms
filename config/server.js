293968e3ea9cf55bd1d025fcec080eb5e1c75c52
edpe <ed@edperkins.co.uk>
2021-10-05 14:42:17 +0100

    feat: setup strapi for performances and locations

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bbca527625c6bda25929bb5cbf3223e3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2d77c54e-9686-4c3d-b99b-aef2f5472b84'
+};
